f7d23db79d30205377d52e6fbadf6284471df160
Syannekaroline <syannekaroline@gmail.com>
2022-12-02 22:35:23 -0300

    lab05 finalizado

--- lab05/RoundRobin.py
+++ lab05/RoundRobin.py
@@ -6,16 +6,92 @@ P1, 200
 P2, 300 
 P3, 25 
 
-"""     
+""" 
+from time import sleep
+
+def GeraFila(ListPrioridades, Processos):
+    '''o descritor do processo é inserido no final da fila correspondente à sua  prioridade.'''
+
+    #declarar dicionário pra armazenar as filas de prioridades
+    FilaPrioridades={}
+
+    #percorre a lista de prioridades -- Organiza pro processos nas filas de acordo com sua prioridade
+    for p in sorted(ListPrioridades): 
+        aux=list()#lista auxiliar pra armazenar dicionários de processos de mesma prioridade
+
+        #percorre a lista de processos
+        for Process,Infos in Processos.items():
+            dictAux=dict()#dicionário pra armazenar temporariamente o um dicionário de processo para ser adicionado em uma lista
+
+            if Infos[1] == p :#verifica se a prioridade do processo é a mesma da lista de prioridades
+                dictAux[Process]= Infos
+                aux.append(dictAux.copy())
+
+        FilaPrioridades[p]=aux
+    return FilaPrioridades
+
+def RoundRobin(FilaPrioridades):
+    print(f"\n\033[32m{'=-'*35}\nROUND ROBIN ALGORITMO\n{'=-'*35}\033[m")
+
+    for p,fila in FilaPrioridades.items():
+        print(f"\n\033[34m{'=-'*35}\nExecutando a fila de prioridade {p}\n{'=-'*35}\033[m")
+        print(f"Fila : {fila}\n")
+        TimeCurrent=TimeSlice
+        ProcessosFinalizados=0
+
+        while ProcessosFinalizados != len(fila):
+            for n, processo in enumerate(fila) :
+                if fila[n]== 0: #verifica se o processo já foi finalizado
+                    continue
+
+                chave=list(processo.keys())[0]
+                timeCPU=list(processo.values())[0][0]
+
+                if TimeCurrent == timeCPU:#
+                    print(f"\033[31m{chave}:{TimeSlice}*\033[m",end="")
+                    fila[n]=0#se o processo foi finalizado ele recebe o valor 0 na fila
+                    ProcessosFinalizados+=1
+
+                elif (timeCPU - TimeCurrent) < 0: 
+                    print(f"\033[31m{chave}:{timeCPU%TimeSlice}* \033[m",end="")
+                    fila[n]=0
+                    ProcessosFinalizados+=1
+
+                else:
+                    print(f"{chave}:{TimeSlice}, ",end="")
+            
+            TimeCurrent+=TimeSlice
+            print("\n")
+            sleep(1)
+
+def FilaUnicaPrioridades(Processos):
+    print(Processos)
+    FilaUnicaProcessos= sorted(Processos.items(), key= lambda item : item[1][1])#organiza os processos de acordo com a prioridade
+    print(f"\n\033[32m{'=-'*35}\nFILA ÚNICA COM PRIORIDADES - ALGORITMO\n{'=-'*35}\033[m")
+    print(f'Fila de processamento : {FilaUnicaProcessos}')
+
+    for processo in FilaUnicaProcessos:
+        TimeCurrent = 0
+        print("\n")
+        while TimeCurrent < processo[1][0] :
+            TimeCurrent+=TimeSlice
+            if TimeCurrent == processo[1][0]:#
+                print(f"\033[31m{processo[0]}:{TimeSlice}*\033[m",end="")
+                break
+            elif processo[1][0]-TimeCurrent < 0: 
+                print(f"\033[31m{processo[0]}:{processo[1][0]%TimeSlice}* \033[m",end="")
+                break
+            else:
+                print(f"{processo[0]}:{TimeSlice}, ",end="")
+            sleep(1)
+
+
+    
 #receber time slice
 TimeSlice = int(input("Time slice: "))
-MaxCPUTime=0
 
-#Declarar um dicionário onde a chave é o progesso e o valor é uma lista contendo o tempo de CPU e a Prioridade
+#Declarar um dicionário onde a chave é o processo e o valor é uma lista contendo o tempo de CPU e a Prioridade
 Processos={}
-
-#declarar dicionário pra armazenar as filas de prioridades
-Prioridades={}
 
 #Lista com as prioridades existentes 
 ListPrioridades=[]
@@ -33,50 +109,13 @@ for i in range (N_processos):
     Processos[f'P{i+1}']= ListAux
     if ListAux[1] not in ListPrioridades : ListPrioridades.append(ListAux[1])
 
-print(ListPrioridades)
+print(f"\nPrioridade(s) existente(s) : {ListPrioridades}")
+
+if len(ListPrioridades) > 1:
+  FilaUnicaPrioridades(Processos)
+    
+FilaPrioridades=GeraFila(ListPrioridades,Processos)
+RoundRobin(FilaPrioridades)
 
 
-#percorre a lista de prioridades
-for p in ListPrioridades: 
 
-    aux=list()#lista auxiliar pra armazenar dicionários de processos de mesma prioridade
-
-    #percorre a lista de processos
-    for Process,Infos in Processos.items():
-        dictAux=dict()#dicionário pra armazenar temporariamente o um dicionário de processo para ser adicionado em uma lista
-
-        if Infos[1] == p :#verifica se a prioridade do processo é a mesma da lista de prioridades
-            dictAux[Process]= Infos
-            aux.append(dictAux.copy())
-
-    Prioridades[p]=aux
-
-for p,fila in Prioridades.items():
-    print(f"Executando a fila de prioridade {p}")
-    print(f"Fila : {fila}\n")
-    TimeCurrent=TimeSlice
-    ProcessosFinalizados=0
-    while True:
-        
-        for n, processo in enumerate(fila) :
-            if fila[n]== 0: #verifica se o processo já foi finalizado
-                continue
-
-            chave=list(processo.keys())[0]
-            timeCPU=list(processo.values())[0][0]
-
-            if TimeCurrent == timeCPU:#
-                print(f"\033[31m{chave}:{TimeSlice}*\033[m",end="")
-                fila[n]=0#se o processo foi finalizado ele recebe o valor 0 na fila
-                ProcessosFinalizados+=1 
-            elif (timeCPU - TimeCurrent) == -(timeCPU%TimeSlice): 
-                print(f"\033[31m{chave}:{timeCPU%TimeSlice}* \033[m",end="")
-                fila[n]=0
-                ProcessosFinalizados+=1
-            else:
-                print(f"{chave}:{TimeSlice}, ",end="")
-        
-        if ProcessosFinalizados == len(fila):
-                break
-        TimeCurrent+=TimeSlice
-        print("\n")
